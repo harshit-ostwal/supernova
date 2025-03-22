6ed66df5bced4316b75fa0429f5cf01912946fc7
Harshit Jain <113440545+harshit-ostwal@users.noreply.github.com>
2025-03-23 01:27:41 +0530

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -24,7 +24,7 @@ function page() {
         {/* <Timeline /> */}
         <About />
         <Tracks />
-        <Sponsors />
+         {/*<Sponsors />*/}
         <Organiser />
         <FAQ />
       </div>
